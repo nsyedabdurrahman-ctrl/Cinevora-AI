fca0df009005b845a602d604376b5cc5cc0f22c3
Syed Abdur Rahman <your-email@gmail.com>
2026-06-09 10:10:11 +0500

    Initial commit

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import Groq from "groq-sdk";
-
+console.log("GROQ KEY EXISTS:", !!process.env.GROQ_API_KEY);
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
